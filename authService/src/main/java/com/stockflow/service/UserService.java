047c749c8085bdd8d2988c9031a0f5f6c98ad6e7
Ojasvita <157558684+Ojasvita33@users.noreply.github.com>
2026-03-18 01:27:16 +0530

    Delete authService directory

--- authService/src/main/java/com/stockflow/service/UserService.java
+++
@@ -1,8 +0,0 @@
-package com.stockflow.service;
-
-import com.stockflow.model.User;
-
-public interface UserService {
-    User register(User user);
-    String login(String username, String password);
-}
